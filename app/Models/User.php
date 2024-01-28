90ca594f14f7623178c15168cb2a3df3f16db1a1
noumanisrar <noumanisrar786@gmail.com>
2024-01-28 15:04:57 +0500

    removed profile_photo_url append property from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,8 +44,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-    
-    protected $appends = [
-        'profile_photo_url',
-    ];
 }
